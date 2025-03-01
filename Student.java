27706bbb62d46705188c0d2bdc6ee085d0bde323
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-01 17:38:51 +0530

    Created a class Student

---
+++ Student.java
@@ -0,0 +1,8 @@
+// Student.java
+// Defines the Student class with attributes and getter/setter methods
+
+class Student {
+    private String name;
+    private long prn;
+    private String dob;
+    private double marks;
