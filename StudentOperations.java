915e2ab393a7c5c1c43d6b47a6db37940a1f7584
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-01 17:59:06 +0530

    Update and Delete Student

--- StudentOperations.java
+++ StudentOperations.java
@@ -62,3 +62,30 @@ class StudentOperations {
             System.out.println("\n Invalid Position! Please enter a valid index.");
         }
     }
+
+    // Update/Edit Student details
+    public void updateStudent(long prn, String new_name, String new_dob, double new_marks) {
+        for (Student student : student_list) {
+            if (student.getPRN() == prn) {
+                student.setName(new_name);
+                student.setDOB(new_dob);
+                student.setMarks(new_marks);
+                System.out.println("\n Student Updated Successfully!");
+                return;
+            }
+        }
+        System.out.println("\n No student found with PRN: " + prn);
+    }
+
+    // Delete student
+    public void deleteStudent(long prn) {
+        for (Student student : student_list) {
+            if (student.getPRN() == prn) {
+                student_list.remove(student);
+                System.out.println("\n Student Deleted Successfully!");
+                return;
+            }
+        }
+        System.out.println("\n No student found with PRN: " + prn);
+    }
+}
