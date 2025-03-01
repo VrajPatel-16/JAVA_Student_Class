0a56eb090d626436a00c2bbfc928727ca990d87a
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-01 17:54:57 +0530

    display students

--- StudentOperations.java
+++ StudentOperations.java
@@ -14,5 +14,17 @@ class StudentOperations {
     // Add a new student
     public void addStudent(Student student) {
         student_list.add(student);
-        System.out.println("\n✅ Student Added Successfully!");
+        System.out.println("\n Student Added Successfully!");
+    }
+
+    // Display all students
+    public void displayStudents() {
+        if (student_list.isEmpty()) {
+            System.out.println("\n No Students Available!");
+        } else {
+            System.out.println("\n Student Records:");
+            for (Student student : student_list) {
+                student.display();
+            }
+        }
     }
